59bfa24988150090a056a56bd4765ba23a357056
ravt3ja <999teja@gmail.com>
2016-08-29 09:29:32 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("myapp", []);
+app.controller("SearchController", function ($scope, $http) {
+    
+    $http.get("https://jsonplaceholder.typicode.com/users")
+  .then(function (response) {     
+      $scope.data = response.data;
+  });
+    
+});
